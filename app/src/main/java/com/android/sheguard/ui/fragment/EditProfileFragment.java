8d1a6b900da0a3ba521094c7434c80037168a8c9
DrDisagree <crazymahmud08@gmail.com>
2023-10-03 21:53:35 +0600

    Refactor and add profile fragment

---
+++ app/src/main/java/com/android/sheguard/ui/fragment/EditProfileFragment.java
@@ -0,0 +1,110 @@
+package com.android.sheguard.ui.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.android.sheguard.R;
+import com.android.sheguard.common.Constants;
+import com.android.sheguard.databinding.FragmentEditProfileBinding;
+import com.android.sheguard.model.UserModel;
+import com.android.sheguard.ui.view.LoadingDialog;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+public class EditProfileFragment extends Fragment {
+
+    private FragmentEditProfileBinding binding;
+    private LoadingDialog loadingDialog;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        binding = FragmentEditProfileBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(binding.header.toolbar);
+        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            binding.header.collapsingToolbar.setTitle(getString(R.string.activity_edit_profile_title));
+            binding.header.collapsingToolbar.setSubtitle(getString(R.string.activity_edit_profile_desc));
+        }
+
+        loadingDialog = new LoadingDialog(getContext());
+
+        binding.btnSave.setOnClickListener(v -> {
+            loadingDialog.show(null);
+            saveDetailsInDatabase();
+        });
+
+        return view;
+    }
+
+    private void saveDetailsInDatabase() {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        CollectionReference UserList = firestore.collection(Constants.FIRESTORE_COLLECTION_USERLIST);
+
+        FirebaseFirestore.getInstance()
+                .collection(Constants.FIRESTORE_COLLECTION_USERLIST)
+                .document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if (document.exists()) {
+                            String name = binding.etNewName.getText() != null ? binding.etNewName.getText().toString().trim() : "";
+                            String phone = binding.etNewPhone.getText() != null ? binding.etNewPhone.getText().toString().trim() : "";
+
+                            UserModel user = new UserModel(
+                                    name.isEmpty() ? document.getString("name") : name,
+                                    document.getString("email"),
+                                    phone.isEmpty() ? document.getString("phone") : phone
+                            );
+
+                            UserList.document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                                    .set(user)
+                                    .addOnCompleteListener(task2 -> {
+                                        loadingDialog.hide();
+
+                                        if (task2.isSuccessful()) {
+                                            binding.etNewName.setText("");
+                                            binding.etNewPhone.setText("");
+
+                                            Snackbar.make(binding.getRoot(), "Details saved successfully", Snackbar.LENGTH_SHORT).show();
+                                        } else {
+                                            if (getContext() != null) {
+                                                Toast.makeText(getContext(), Objects.requireNonNull(task2.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                        }
+                    } else {
+                        loadingDialog.hide();
+
+                        if (getContext() != null) {
+                            Toast.makeText(getContext(), Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onDestroyView() {
+        loadingDialog.hide();
+        super.onDestroyView();
+    }
+}
--- app/src/main/java/com/android/sheguard/ui/fragment/ProfileFragment.java
+++ app/src/main/java/com/android/sheguard/ui/fragment/ProfileFragment.java
@@ -1,26 +1,76 @@
 package com.android.sheguard.ui.fragment;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.android.sheguard.R;
+import com.android.sheguard.common.Constants;
 import com.android.sheguard.databinding.FragmentProfileBinding;
+import com.android.sheguard.model.UserModel;
 import com.android.sheguard.ui.activity.LoginRegisterActivity;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.Objects;
 
 public class ProfileFragment extends Fragment {
 
     private FragmentProfileBinding binding;
+    private LocationManager locationManager = null;
+    private LocationRequest locationRequest = null;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentProfileBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(binding.header.toolbar);
+        ActionBar actionBar = ((AppCompatActivity) requireActivity()).getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setDisplayShowHomeEnabled(true);
+            binding.header.collapsingToolbar.setTitle(getString(R.string.activity_profile_title));
+            binding.header.collapsingToolbar.setSubtitle(getString(R.string.activity_profile_desc));
+        }
+
+        if (locationRequest == null) {
+            locationRequest = new LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 5000)
+                    .setWaitForAccurateLocation(false)
+                    .setMinUpdateIntervalMillis(2000)
+                    .setMaxUpdateDelayMillis(5000)
+                    .build();
+        }
+
+        getUserDetails();
+        getCurrentLocation();
+
+        binding.btnEditProfile.setOnClickListener(v -> Navigation.findNavController(view).navigate(R.id.action_profileFragment_to_editProfileFragment));
 
         binding.btnLogout.setOnClickListener(v -> {
             FirebaseAuth.getInstance().signOut();
@@ -30,5 +80,103 @@ public class ProfileFragment extends Fragment {
         });
 
         return view;
+    }
+
+    private void getUserDetails() {
+        FirebaseFirestore.getInstance()
+                .collection(Constants.FIRESTORE_COLLECTION_USERLIST)
+                .document(Objects.requireNonNull(FirebaseAuth.getInstance().getCurrentUser()).getUid())
+                .get()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        if (document.exists()) {
+                            UserModel user = new UserModel(
+                                    document.getString("name"),
+                                    document.getString("email"),
+                                    document.getString("phone")
+                            );
+
+                            binding.tvName.setText(user.getName());
+                            binding.tvEmail.setText(user.getEmail());
+                            binding.tvPhone.setText(user.getPhone());
+                        }
+                    }
+                });
+    }
+
+    private boolean isGPSEnabled() {
+        if (locationManager == null) {
+            locationManager = (LocationManager) requireContext().getSystemService(Context.LOCATION_SERVICE);
+        }
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+
+    private void getCurrentLocation() {
+        if (!isGPSEnabled()) {
+            binding.tvLocation.setText("GPS is not enabled");
+            return;
+        } else {
+            binding.tvLocation.setText("Getting location...");
+        }
+
+        if (ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+
+        final int[] numberOfUpdates = {0};
+
+        LocationServices.getFusedLocationProviderClient(requireContext())
+                .requestLocationUpdates(locationRequest, new LocationCallback() {
+                    @Override
+                    public void onLocationResult(@NonNull LocationResult locationResult) {
+                        super.onLocationResult(locationResult);
+                        if (getContext() == null) {
+                            return;
+                        }
+
+                        numberOfUpdates[0]++;
+
+                        if (numberOfUpdates[0] >= 5) {
+                            LocationServices.getFusedLocationProviderClient(getContext())
+                                    .removeLocationUpdates(this);
+
+                            if (locationResult.getLocations().size() > 0) {
+                                int idx = locationResult.getLocations().size() - 1;
+                                double latitude = locationResult.getLocations().get(idx).getLatitude();
+                                double longitude = locationResult.getLocations().get(idx).getLongitude();
+
+                                Geocoder geocoder;
+                                List<Address> addresses;
+                                geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+                                try {
+                                    addresses = geocoder.getFromLocation(latitude, longitude, 1);
+
+                                    if (addresses != null) {
+                                        StringBuilder address = new StringBuilder();
+
+                                        for (int i = 0; i <= addresses.get(0).getMaxAddressLineIndex(); i++) {
+                                            address.append(addresses.get(0).getAddressLine(i));
+
+                                            if (i < addresses.get(0).getMaxAddressLineIndex()) {
+                                                address.append("\n");
+                                            }
+                                        }
+
+                                        binding.tvLocation.setText(address.toString());
+                                    } else {
+                                        binding.tvLocation.setText("Failed to get location");
+                                    }
+                                } catch (IOException e) {
+                                    binding.tvLocation.setText("Failed to get location");
+                                    e.printStackTrace();
+                                }
+                            }
+                        }
+                    }
+                }, Looper.getMainLooper());
     }
 }
